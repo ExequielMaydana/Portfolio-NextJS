30e2e306a1956e22d6f3a2603713bf81e11646b8
Exe Developer <exedevcoding22@gmail.com>
2023-05-20 12:01:17 -0300

    add btn whats contact

--- src/components/contact/PageContact.jsx
+++ src/components/contact/PageContact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import styles from "../../styles/PageContact.module.css";
 import { useRouter } from "next/router";
 import Form from "./Form";
+import Link from "next/link";
 
 const PageContact = () => {
   const router = useRouter();
@@ -19,18 +20,22 @@ const PageContact = () => {
         - <p>Contacto</p>
       </header>
       <div className={styles.pageContact__ctrTextAndForm}>
-      <article className={styles.pageContact__text}>
-        <h2 className={styles.pageContact__title}>Contacto</h2>
-        <p className={styles.pageContact__subtitle}>
-          Si tienes algun proyecto en el que quieras que colabore o eres alguien
-          que necesita de mis habilidades satisfacer las necesidades de tus
-          clientes, no dudes en enviarme un mensaje, intentaré responder todos
-          los mensajes lo más pronto posible.
-        </p>
-      </article>
-      <Form/>
+        <article className={styles.pageContact__text}>
+          <h2 className={styles.pageContact__title}>Contacto</h2>
+          <p className={styles.pageContact__subtitle}>
+            Si tienes algun proyecto en el que quieras que colabore o eres
+            alguien que necesita de mis habilidades satisfacer las necesidades
+            de tus clientes, no dudes en enviarme un mensaje, intentaré
+            responder todos los mensajes lo más pronto posible.
+          </p>
+        </article>
+        <Form />
       </div>
-    
+
+      <Link href="https://wa.link/is1b74" target="_blank" className={styles.pageContact__what}>
+        {" "}
+        <i className="bx bxl-whatsapp"></i>
+      </Link>
     </section>
   );
 };
