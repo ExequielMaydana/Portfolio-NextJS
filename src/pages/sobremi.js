f4174c0e62afcb8496ddefff35e74fea7febe01f
Exe Developer <exedevcoding22@gmail.com>
2023-05-20 12:10:39 -0300

    delete loading about

--- src/pages/sobremi.js
+++ src/pages/sobremi.js
@@ -1,31 +1,16 @@
 import PageContent from "@/components/about/PageContent";
-import LoadingLine from "@/components/loading/LoadingLine";
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 const Sobremi = () => {
-  const [renderOn, setRenderOn] = useState(true);
-
-  useEffect(() => {
-    setTimeout(() => {
-      setRenderOn(false);
-    }, 3000);
-  }, []);
-
   return (
     <>
-      {renderOn ? (
-        <LoadingLine />
-      ) : (
-        <>
-          <div className="page">
-            <PageContent />
-          </div>
-          <article>
-            <div className="squares1"></div>
-            <div className="squares2"></div>
-          </article>
-        </>
-      )}
+      <div className="page">
+        <PageContent />
+      </div>
+      <article>
+        <div className="squares1"></div>
+        <div className="squares2"></div>
+      </article>
     </>
   );
 };
