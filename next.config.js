461f7ed5c28721baa66c1c15dbb36212aec93870
Exe Developer <exedevcoding22@gmail.com>
2023-07-24 20:57:48 -0300

    add env nextj

--- next.config.js
+++ next.config.js
@@ -2,7 +2,16 @@
 
 const nextConfig = {
   reactStrictMode: true,
-  env: {},
+  env: {
+    CLIENT_ID:
+      "1007429619886-dpl6t1cpmvoch4650aaiem5o960mlfim.apps.googleusercontent.com",
+    CLIENTE_SECRET: "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",
+    REDIRECT_URI: "https://developers.google.com/oauthplayground",
+    REFRESH_TOKEN:
+      "1//04idsfNTuWAjICgYIARAAGAQSNwF-L9IrwQNivbXHP9dxcRNQFzFQUJnIMPXDdc8RQO0d5qsuJ0Fvr-XLLIsrEVL9tcuCZzWiaxc",
+    MY_EMAIL: "exedevcoding22@gmail.com",
+    NEXT_PUBLIC_API_BASE_URL: "https://exedeveloper.me",
+  },
 };
 
 module.exports = nextConfig;
