90756a3141e9c97caffd5c47050de4a73a484237
ExequielMaydana <exedevcoding22@gmail.com>
2023-07-27 21:18:05 -0300

    edit cv

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    CLIENT_ID:
+      "1007429619886-dpl6t1cpmvoch4650aaiem5o960mlfim.apps.googleusercontent.com",
+    CLIENTE_SECRET: "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",
+    REDIRECT_URI: "https://developers.google.com/oauthplayground",
+    REFRESH_TOKEN:
+      "1//04idsfNTuWAjICgYIARAAGAQSNwF-L9IrwQNivbXHP9dxcRNQFzFQUJnIMPXDdc8RQO0d5qsuJ0Fvr-XLLIsrEVL9tcuCZzWiaxc",
+    MY_EMAIL: "exedevcoding22@gmail.com",
+  },
 };
 
 module.exports = nextConfig;
